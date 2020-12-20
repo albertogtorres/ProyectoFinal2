6f01e177e8b6399b29e3f5f45f8519b56c52c630
alberotgtorres <holasoyalbertoluisgt@gmail.com>
2020-12-20 17:55:11 -0500

    Proyecto final 2

---
+++ js/alert.js
@@ -0,0 +1,9 @@
+function enviar(){
+    Swal.fire({
+        position: 'top-end',
+        icon: 'success',
+        title: 'Has enviado el mensaje',
+        showConfirmButton: false,
+        timer: 1500
+      })
+      }
---
+++ js/menu.js
@@ -0,0 +1,9 @@
+
+$(".nav-link").mouseover(function(){
+        $(this).css("font-size", "25px" )
+    }
+)
+$(".nav-link").mouseout(function(){
+    $(this).css("font-size", "20px" )
+}
+)
